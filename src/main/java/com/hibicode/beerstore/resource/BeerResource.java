af3cf5006b00c62278525aed6621f745a03733ea
Alexander Souza <alexpt2000@gmail.com>
2019-04-22 19:51:54 +0100

    Add valiation, getAll and Save

--- src/main/java/com/hibicode/beerstore/resource/BeerResource.java
+++ src/main/java/com/hibicode/beerstore/resource/BeerResource.java
@@ -1,9 +1,12 @@
 package com.hibicode.beerstore.resource;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.hibicode.beerstore.model.Beer;
+import com.hibicode.beerstore.repository.Beers;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.List;
 
@@ -11,8 +14,17 @@ import java.util.List;
 @RequestMapping("/beers")
 public class BeerResource {
 
+    @Autowired
+    private Beers beers;
+
     @GetMapping
-    public List<String> all() {
-        return Arrays.asList("Heineken", "Colorado Indiga", "Stella Artois", "Bohemia", "Skol");
+    public List<Beer> all() {
+        return beers.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Beer create(@Valid @RequestBody Beer beer){
+        return beers.save(beer);
     }
 }
