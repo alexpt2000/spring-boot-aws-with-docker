accc801da3336bc2514a12f17c0040c0749abe2d
Alexander Souza <alexpt2000@gmail.com>
2019-04-27 21:32:47 +0100

    Create test with Hamcrest

---
+++ src/test/java/com/hibicode/beerstore/service/BeerServiceTest.java
@@ -0,0 +1,46 @@
+package com.hibicode.beerstore.service;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+import java.math.BigDecimal;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.hibicode.beerstore.model.Beer;
+import com.hibicode.beerstore.model.BeerType;
+import com.hibicode.beerstore.service.exception.BeerAlreadyExistException;
+
+public class BeerServiceTest {
+
+	private BeerService beerService;
+
+	@Before
+	public void setup() {
+		beerService = new BeerService();
+	}
+
+	@Test(expected = BeerAlreadyExistException.class)
+	public void should_deny_creation_of_beer_that_exists() {
+		Beer newBeer = new Beer();
+		newBeer.setName("Heineken");
+		newBeer.setType(BeerType.LAGER);
+		newBeer.setVolume(new BigDecimal("355"));
+		beerService.save(newBeer);
+	}
+
+	@Test
+	public void should_create_new_beer() {
+		Beer newBeer = new Beer();
+		newBeer.setName("Heineken");
+		newBeer.setType(BeerType.LAGER);
+		newBeer.setVolume(new BigDecimal("355"));
+
+		Beer beerSaved = beerService.save(newBeer);
+
+		assertThat(beerSaved.getId(), equalTo(10L));
+		assertThat(beerSaved.getName(), equalTo("Heineken"));
+		assertThat(beerSaved.getType(), equalTo(BeerType.LAGER));
+	}
+}
