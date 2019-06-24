22faf1495af830d6a8864f0d70978b52e71b8653
Alexander Souza <alexpt2000@gmail.com>
2019-06-24 18:18:52 +0100

    Criando chave assimétrica para acessar as instâncias

--- terraform/instances.tf
+++ terraform/instances.tf
@@ -1,11 +1,21 @@
-resource "aws_instance" "instance" {
+resource "aws_key_pair" "keypair" {
+  public_key = "${file("key/beerstore_key.pub")}}"
+}
+
+resource "aws_instance" "instances" {
   count = 3
 
   ami = "ami-0cc96feef8c6bbff3"
   instance_type = "t2.micro"
   subnet_id = "${element(aws_subnet.public_subnet.*.id, count.index)}"
 
+  key_name = "${aws_key_pair.keypair.key_name}"
+
   tags {
     Name = "hibicode_instances"
   }
+}
+
+output "public_ips" {
+  value = "${join(", ", aws_instance.instances.*.public_ip)}"
 }
